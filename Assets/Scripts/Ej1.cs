836ab51b8e321ad2bda3e2b2c54e55f76615f43a
Bautista2049 <49379221@est.ort.edu.ar>
2025-05-08 10:04:08 -0300

    Toda la prueba

---
+++ Assets/Scripts/Ej1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej1 : MonoBehaviour
+{ 
+    public int num1;
+    public int num2;
+    int numMayor;
+    // Start is called before the first frame update
+    void Start()
+    {
+      if (num1 > num2)
+      {
+        Debug.Log("El mayor de los dos numeros ingresados es " + num1);
+        return;
+      };
+      if (num2 > num1)
+      {
+        Debug.Log("El mayor de los dos numeros ingresados es " + num2);
+        return;
+      };
+        if (num1 == num2)
+        {
+          Debug.Log("Son iguales");
+          return;
+        }
+      
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Ej2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej2 : MonoBehaviour
+{
+    string palabraCorrecta = "domingo";
+    public string palabra;
+    // Start is called before the first frame update
+    void Start()
+    {
+      if (palabra == palabraCorrecta)
+        {
+          Debug.Log("Has adivinado la palabra");
+          return;
+        }
+      else
+        {
+            Debug.Log("Palabra incorrecta");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
